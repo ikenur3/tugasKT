69d147346e27a3624107782b7164f18e1d18f57c
ikenur3 <ikenurhayati6661@gmail.com>
2016-09-22 13:21:47 +0700

    Add files via upload
    arduino uno

---
+++ xitkj2013.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // intialize digital pin 2 as an output.
+    pinMode(2,OUTPUT);
+}
+
+void loop() {
+  digitalWrite (2,HIGH);
+  delay(3500);
+  digitalWrite (2,LOW);
+  delay(3500); 
+}
